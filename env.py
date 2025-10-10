706300a87e021203a9816411b04aa7e609a7c94c
Mubashirgit1 <mubashir.connect@gmail.com>
2025-10-10 18:52:37 +0100

    connect database to project

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_I4VTvfjHe8uw@ep-soft-shadow-agmr1n0u.c-2.eu-central-1.aws.neon.tech/broke_stack_scrub_392987")
--- pockettrip/settings.py
+++ pockettrip/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ud7yq!g)^-qx%_(+&bjj$90iucns!kppa$rvpaf)r2k*7*(5qs'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -75,13 +79,15 @@ WSGI_APPLICATION = 'pockettrip.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
