3558e2028339e73cc5bd10a20675f5ffd2b00a80
Mubashirgit1 <mubashir.connect@gmail.com>
2025-10-14 19:50:58 +0100

    save the search history of user flights

--- searchtrip/admin.py
+++ searchtrip/admin.py
@@ -4,3 +4,21 @@ from django.contrib import admin
 from .models import SearchFilter
 
 admin.site.register(SearchFilter)
+class SearchFilterAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 
+        'departure_city', 
+        'departure_airport_code',
+        'arrival_city', 
+        'arrival_airport_code',
+        'departure_date', 
+        'return_date',
+        'created_at'
+    )
+    search_fields = (
+        'departure_city', 
+        'arrival_city', 
+        'departure_airport_code',
+        'arrival_airport_code',
+        'user__username'
+    )
